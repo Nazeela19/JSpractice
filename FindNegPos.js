0d5bc4a7651cc34b3054dc3d717ea604e94d20fb
nazeelaa <nazeelakudari949@gmail.com>
2022-03-24 14:30:29 +0530

    program to find positive or negative number

---
+++ FindNegPos.js
@@ -0,0 +1,17 @@
+ //code To check whether number is positive or negative
+ 
+
+const number = 101;
+
+if ( number === 0 ) 
+{
+    alert("It's a Zero");
+} 
+else if ( number >= 1 ) 
+{
+    alert("It's a Positive Number");
+} 
+else 
+{
+    alert("It's a Negative Number");
+}
