2d80a55a7966cc1b77cd2bb6811f33ae5e427323
nazeelaa <nazeelakudari949@gmail.com>
2022-03-25 12:20:47 +0530

    multiplications of 8 code added

---
+++ MultiplesOfEight.js
@@ -0,0 +1,20 @@
+//program to print multiplication of 8
+
+for(var i = 1; i <= 10; i++)
+{
+   var row="8 * " +i+ " = " + 8*i;
+  // var row="7*"+i+"="+7*i;
+    console.log(row);
+}
+
+//output:
+8 * 1 = 8
+8 * 2 = 16
+8 * 3 = 24
+8 * 4 = 32
+8 * 5 = 40
+8 * 6 = 48
+8 * 7 = 56
+8 * 8 = 64
+8 * 9 = 72
+8 * 10 = 80
