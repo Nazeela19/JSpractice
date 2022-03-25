c4f3f13b51c3ae2c4d53248e5a35da0604811ae4
nazeelaa <nazeelakudari949@gmail.com>
2022-03-25 15:37:03 +0530

    //Reverse an array
    var ar = [1, 2, 3];
    var ar2 = reverseArray(ar);
    console.log(ar2);
    
    function reverseArray(ar)
    {
        var ar2 = [];
    
        for(var i = ar.length - 1; i >= 0; i--)
        {
            ar2.push(ar[i]);
        }
    
        return ar2;
    }
    
    output:

---
+++ reverseArray.js
@@ -0,0 +1,20 @@
+//Reverse an array
+
+var ar = [1, 2, 3];
+var ar2 = reverseArray(ar);
+console.log(ar2);
+
+function reverseArray(ar)
+{
+    var ar2 = [];
+    
+    for(var i = ar.length - 1; i >= 0; i--)
+    {
+        ar2.push(ar[i]);
+    }
+    
+    return ar2;
+}
+
+output:
+[ 3, 2, 1 ]
